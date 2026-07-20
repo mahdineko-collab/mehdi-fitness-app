98f56596c8f582ac11b814a4f32a9b3513e0cf22
mahdineko-collab <mahdi.neko@gmail.com>
2026-07-20 23:56:37 +0330

    Add files via upload
    نسخه اولیه اپ تمرینی

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE='mehdi-fitness-v2';
+const ASSETS=['./','./index.html','./manifest.json','./assets/icon-192.png','./assets/icon-512.png','./assets/apple-touch-icon.png'];
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
+  const url=new URL(event.request.url);
+  if(url.origin!==self.location.origin) return;
+  event.respondWith(
+    caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{
+      if(response && response.status===200){
+        const copy=response.clone();
+        caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+      }
+      return response;
+    }).catch(()=>caches.match('./index.html')))
+  );
+});
